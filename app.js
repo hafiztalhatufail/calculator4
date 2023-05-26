c5731bfd19b0fe98eba11e281d86cd826e6d1dc2
hafiztalhatufail <134297558+hafiztalhatufail@users.noreply.github.com>
2023-05-26 19:04:31 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var display = document.getElementById("display");
+
+
+function getValue(num){
+   display.value += num;
+  console.log(display.value);
+}
+function calculateValue(){}
+function clearAll(){}
+    
+   
